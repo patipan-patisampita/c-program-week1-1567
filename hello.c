81c7a1c353c105f5cbe156b0c9b628b01125b7dc
patipan <patipan.patisampita@gmail.com>
2024-05-24 15:37:31 +0700

    week1-cprogram

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 18;
+    //Print Hello World to the screen
+    printf("Hello World!\n");
+    printf("My Name is Mr.John Doe!\n");
+    printf("Age: %d\n", age);
+    return 0;
+}
